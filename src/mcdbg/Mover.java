607c9574375560603138f793f0ab725e939f6c9b
Alex Adams <alex-j-a@hotmail.co.uk>
2013-03-06 06:27:11 +0000

    Debugged mcdbg

--- src/mcdbg/Mover.java
+++ src/mcdbg/Mover.java
@@ -1,102 +1,266 @@
 package mcdbg;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class Mover extends Thread {
 	private enum Mode {
-		IDLE, TASK_A, TASK_B, TASK_C
+		STOP, DELAY, MOVE_VECTOR, MOVE_TO
 	};
 
-	private Mode mode = Mode.IDLE;
+	private boolean running = false;
+	private boolean interruptMove = false;
 	private boolean die = false;
+
+	private class MoverConfig {
+		public double x = 0;
+		public double y = 0;
+
+		public long milliseconds = 0;
+
+		public Mode mode;
+	};
+
+	private ConcurrentLinkedQueue<MoverConfig> moveQueue = new ConcurrentLinkedQueue<MoverConfig>();
+	private Semaphore queueSem = new Semaphore(1, true);
 
 	private Semaphore jobSem = new Semaphore(0, true);
 	private Semaphore killSem = new Semaphore(0, true);
 	private Semaphore waitSem = new Semaphore(0, true);
 
+	/** Thread-safe sleep scheduler */
+	private final ScheduledExecutorService sleepScheduler = Executors
+			.newScheduledThreadPool(1);
+
+	/** Thread-safe sleep */
+	private void safeSleep(long millis) throws InterruptedException {
+		final Semaphore sleepSem = new Semaphore(0, true);
+		// Schedule a wake-up after the specified time
+		sleepScheduler.schedule(new Runnable() {
+			@Override
+			public void run() {
+				sleepSem.release();
+			}
+		}, millis, TimeUnit.MILLISECONDS);
+		// Wait for the wake-up
+		sleepSem.acquire();
+	}
+
+	private boolean pushMovement(MoverConfig movement) {
+		int pushAttempts = 0;
+		try {
+			queueSem.acquire();
+		} catch (InterruptedException e) {
+			return false;
+		}
+		// Try to push the movement 10 times before giving up
+		while (!moveQueue.offer(movement) && pushAttempts < 10)
+			++pushAttempts;
+		queueSem.release();
+		// If we gave up, return false to indicate it
+		if (pushAttempts >= 10)
+			return false;
+		return true;
+	}
+
 	public Mover() {
 		super("mover");
 	}
-	
+
 	@Override
 	public void run() {
-		System.out.println(Thread.currentThread().getName() + ": Confirmed start");
+		// safeSleep requires some initial use to prevent lag spikes on the
+		// first few runs - Thread.sleep has similar problems but less
+		// noticeable
+		try {
+			for (int i = 0; i < 3; ++i)
+				safeSleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		try {
 			while (!die) {
-				System.out.println(Thread.currentThread().getName() + ": Acquiring jobSem");
+				// Temporarily block queue changes while retrieving the next job
+				queueSem.acquire();
+				// Wait for next movement operation
 				jobSem.acquire();
-				System.out.println(Thread.currentThread().getName() + ": Acquired jobSem");
-				switch (mode) {
-				case IDLE:
-					break;
-				case TASK_A:
-					doTaskA();
-					break;
-				case TASK_B:
-					doTaskB();
-					break;
-				case TASK_C:
-					doTaskC();
-					break;
-				default:
-					System.out.println("DERP! Unknown movement mode specified");
-					assert (false);
+				// Clear the movement interrupt flag for the new movement
+				interruptMove = false;
+				// Set the running flag to true for busy-waiting
+				running = true;
+
+				if (!moveQueue.isEmpty() && !die) {
+					MoverConfig movement = moveQueue.poll();
+					// Queue will not change from here on, so release the
+					// semaphore
+					queueSem.release();
+					assert (movement != null) : "moveQueue.poll() returned null when non-empty";
+
+					if (movement.mode == null) {
+						System.out.println("Mover is idle");
+						continue;
+					}
+
+					switch (movement.mode) {
+					case STOP:
+						System.out.println("Stopping robot");
+						break;
+					case DELAY:
+						System.out.println("Waiting for "
+								+ movement.milliseconds + " milliseconds");
+						safeSleep(movement.milliseconds);
+						break;
+					case MOVE_VECTOR:
+						System.out.println("Moving at speed (" + movement.x
+								+ ", " + movement.y + ")");
+						doMove(movement.x, movement.y);
+						break;
+					case MOVE_TO:
+						System.out.println("Moving to point (" + movement.x
+								+ ", " + movement.y + ")");
+						doMoveTo(movement.x, movement.y);
+						break;
+					default:
+						System.out
+								.println("DERP! Unknown movement mode specified");
+						assert (false);
+					}
+				} else {
+					queueSem.release();
 				}
-				if (waitSem.hasQueuedThreads()) {
-					System.out.println(Thread.currentThread().getName() + ": Releasing waitSem");
-					waitSem.release();
-					System.out.println(Thread.currentThread().getName() + ": Released waitSem");
+				
+				// If we just did the last move in the queue, wake up the waiters
+				if (moveQueue.isEmpty()) {
+					// Only wake up the waiting threads if there are waiting
+					// threads to wake up
+					System.out.println("Waking up waiters");
+					while (waitSem.hasQueuedThreads())
+						waitSem.release();
+					running = false;
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(Thread.currentThread().getName() + ": Releasing killSem");
-		killSem.release();
+		// Signal that robot is stopped and safe to disconnect
+		// Only if there is actually any threads waiting
+		while (killSem.hasQueuedThreads())
+			killSem.release();
 	}
 
 	public void kill() {
 		die = true;
-		System.out.println(Thread.currentThread().getName() + ": Acquiring killSem");
+		interruptMove = true;
+		// Trigger one final run for the mover thread to clean up waiters, stop
+		// the robot and clear the robot's buffer
+		jobSem.release();
+		// Wait for the thread to die
 		killSem.acquireUninterruptibly();
 	}
 
 	public void waitForCompletion() throws InterruptedException {
-		System.out.println(Thread.currentThread().getName() + ": Acquiring waitSem");
 		waitSem.acquire();
-		System.out.println(Thread.currentThread().getName() + ": Acquired waitSem");
 	}
 
-	public void taskA() {
-		mode = Mode.TASK_A;
-		System.out.println(Thread.currentThread().getName() + ": Releasing jobSem");
+	public void interruptMove() {
+		interruptMove = true;
+	}
+	
+	public void resetQueue() throws InterruptedException {
+		if (moveQueue.isEmpty())
+			return;
+		interruptMove();
+
+		// Block changes in the queue until the queue is finished
+		// resetting
+		queueSem.acquire();
+		// Acquire the permits for queued jobs to cancel the execution for them
+		jobSem.acquire(moveQueue.size());
+		moveQueue.clear();
+		// Reactivate the movement thread
+		queueSem.release();
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public boolean hasQueuedJobs() {
+		// Semaphore not required since isEmpty is constant runtime and the
+		// queue is thread-safe
+		return !moveQueue.isEmpty();
+	}
+
+	public int numQueuedJobs() {
+		// Get a lock on the queue to prevent changes while determining how many
+		// jobs there are
+		try {
+			queueSem.acquire();
+			int result = moveQueue.size();
+			queueSem.release();
+			return result;
+		} catch (InterruptedException e) {
+			return 0;
+		}
+	}
+
+	public synchronized boolean move(double speedX, double speedY) {
+		MoverConfig movement = new MoverConfig();
+		movement.x = speedX;
+		movement.y = speedY;
+		movement.mode = Mode.MOVE_VECTOR;
+
+		if (!pushMovement(movement))
+			return false;
+
+		// Let the mover know it has a new job
 		jobSem.release();
-		System.out.println(Thread.currentThread().getName() + ": Released jobSem");
+		return true;
 	}
 
-	private void doTaskA() {
-		System.out.println("Doing task A!");
+	private void doMove(double speedX, double speedY) {
+		System.out.println("Doing move: X=" + speedX + ", Y=" + speedY);
 	}
 
-	public void taskB() {
-		mode = Mode.TASK_B;
-		System.out.println(Thread.currentThread().getName() + ": Releasing jobSem");
+	public synchronized boolean moveTo(double x, double y) {
+		MoverConfig movement = new MoverConfig();
+		movement.x = x;
+		movement.y = y;
+		movement.mode = Mode.MOVE_TO;
+
+		if (!pushMovement(movement))
+			return false;
+
+		// Let the mover know it has a new job
 		jobSem.release();
-		System.out.println(Thread.currentThread().getName() + ": Released jobSem");
+		return true;
 	}
 
-	private void doTaskB() {
-		System.out.println("Doing task B!");
+	private void doMoveTo(double x, double y) {
+		System.out.println("Doing moveTo: X=" + x + ", Y=" + y);
+		for (int i = 0; i < 50; ++i) {
+			try {
+				safeSleep(42);
+			} catch (InterruptedException e) {
+				System.out.println("Failed to sleep");
+				e.printStackTrace();
+			}
+		}
 	}
 
-	public void taskC() {
-		mode = Mode.TASK_C;
-		System.out.println(Thread.currentThread().getName() + ": Releasing jobSem");
+	public synchronized boolean delay(long milliseconds) {
+		MoverConfig movement = new MoverConfig();
+		movement.milliseconds = milliseconds;
+		movement.mode = Mode.DELAY;
+
+		if (!pushMovement(movement))
+			return false;
+
+		// Let the mover know it has a new job
 		jobSem.release();
-		System.out.println(Thread.currentThread().getName() + ": Released jobSem");
-	}
-
-	private void doTaskC() {
-		System.out.println("Doing task C!");
+		return true;
 	}
 }
--- src/mcdbg/Runner.java
+++ src/mcdbg/Runner.java
@@ -7,23 +7,39 @@ public class Runner {
 		mover.start();
 		System.out.println("Mover thread started");
 		try {
-			System.out.println("Calling taskA");
-			mover.taskA();
-			System.out.println("Calling wait");
-			mover.waitForCompletion();
-			System.out.println("Calling taskB");
-			mover.taskB();
-			System.out.println("Calling wait");
-			mover.waitForCompletion();
-			System.out.println("Calling taskC");
-			mover.taskC();
-			System.out.println("Calling wait");
-			mover.waitForCompletion();
-			System.out.println("Calling kill");
+			for (int i = 0; i < 10; ++i) {
+				int x = 10 * i, y = 100 - x;
+				System.out.println("Adding move to queue: X=" + x + ", Y=" + y);
+				mover.move(x, y);
+				System.out.println("Adding delay to queue");
+				mover.delay(500);
+			}
+			System.out.println("Resetting Queue...");
+			mover.resetQueue();
+			for (int i = 0; i < 10; ++i) {
+				int x = 10 * i, y = 100 - x;
+				System.out.println("Adding moveTo to queue: X=" + x + ", Y=" + y);
+				mover.moveTo(x, y);
+				System.out.println("Adding delay to queue");
+				mover.delay(500);
+			}
+			System.out.println("Queued moves: " + mover.numQueuedJobs());
+			System.out.println("Waiting for completion...");
+			System.out.println();
+			
+			Thread.sleep(1000);
+			System.out.println("Resetting Queue...");
+			mover.resetQueue();
+			
+			if (mover.hasQueuedJobs())
+				mover.waitForCompletion();
+			System.out.println();
+			System.out.println("Completed!\nKilling mover");
 			mover.kill();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("Completed successfully!");
 	}
 
 }
